84eae2138ba4b11d16f36d43e35c19c2d671dae0
Thanghv37 <10diembvda@gmail.com>
2026-05-05 08:42:03 +0700

    serve media files with gunicorn

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-
+from django.urls import re_path
+from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
@@ -21,4 +21,10 @@ urlpatterns = [
     path('registrations/', include('registrations.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(
+        r'^media/(?P<path>.*)$',
+        serve,
+        {'document_root': settings.MEDIA_ROOT}
+    ),
+]
